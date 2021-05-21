dd955685e611169fe6d013b1660aa09a85a0c23a
Anarkho <alex.soares1414@gmail.com>
2021-05-21 07:30:24 -0300

    criando os primeiros componentes e implementanto as primeiras rotas

---
+++ src/views/produtos/Cadastro.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+
+export default class CadastroProdutos extends Component {
+    render(){
+        return(
+            <div className='card'>
+                <div className='card-header'>Cadastro de Produtos</div>
+                <div className='card-body'>
+
+                </div>
+
+            </div>
+        )
+    }
+}
