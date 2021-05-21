f1ba41083c9a58186ae66464290f81a339d78818
Anarkho <alex.soares1414@gmail.com>
2021-05-21 12:16:01 -0300

    implementando navegação entre as telas

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import React from 'react'
 import NavBar from './components/Navbar'
 import Routes from './routes/routes'
 
+import {HashRouter} from 'react-router-dom'
+
 
 function App() {
   return (
-    <>
+    <HashRouter>
       <div className='container'> 
       <NavBar></NavBar>
       <Routes></Routes>
       </div>
-    </>
+    </HashRouter>
   );
 }
 
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { HashRouter, Switch, Route } from 'react-router-dom'
+import {Switch, Route } from 'react-router-dom'
 
 import Home from '../components/Home'
 import CadastroProdutos from '../views/produtos/Cadastro'
@@ -7,13 +7,11 @@ import ConsultaProdutos from '../views/produtos/Consulta'
 
 export default () => {
     return(
-        <HashRouter>
             <Switch>
                 <Route exact path='/cadastro-produtos' component={CadastroProdutos}/>
                 <Route exact path='/consulta-produtos' component={ConsultaProdutos}/>
                 <Route exact path='/' component={Home}/>
             </Switch>
-        </HashRouter>
     )
 }
 
